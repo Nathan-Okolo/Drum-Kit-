e879a73b950f0dab5ed405e7c988c600580e0aa9
Okolo Nathan <okolonathanchinedu1@gmail.com>
2022-04-04 06:22:42 +0100

    the intended initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const numberOfDrum = document.querySelectorAll(".drum").length
+for (let i = 0; i < numberOfDrum; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick)
+}
+
+function handleClick() {
+    const button = this.innerHTML
+    buttonAnimation(button)
+    makeSound(button)
+} 
+
+document.addEventListener("keypress", function (event) {
+    buttonAnimation(event.key)
+    makeSound(event.key)
+})
+
+function buttonAnimation(currentKey) {
+    const activeButton = document.querySelector(`.${currentKey}`)
+    activeButton.classList.add("pressed")
+
+    setTimeout(() => {
+        activeButton.classList.remove("pressed")
+    }, 100);
+    
+}
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            const crash = new Audio("/sounds/crash.mp3")
+            crash.play()
+            break;
+        case "a":
+            const kick = new Audio("/sounds/kick-bass.mp3")
+            kick.play()
+            break;
+        case "s":
+            const snare = new Audio("/sounds/snare.mp3")
+            snare.play()
+            break;
+        case "d":
+            const tom1 = new Audio("/sounds/tom-1.mp3")
+            tom1.play()
+            break;
+        case "j":
+            const tom2 = new Audio("/sounds/tom-2.mp3")
+            tom2.play()
+            break;
+        case "k":
+            const tom3 = new Audio("/sounds/tom-3.mp3")
+            tom3.play()
+            break;
+        case "l":
+            const tom4 = new Audio("/sounds/tom-4.mp3")
+            tom4.play()
+            break;
+    
+        default:
+            break;
+    }
+}
